f6b80a3f3d0f6e650a70811911a127ea3e16e980
bignrn <raiyo2580@gmail.com>
2022-11-24 22:20:08 +0900

    マイページのコンテンツを追加

--- src/views/MyHistoryView.vue
+++ src/views/MyHistoryView.vue
@@ -13,7 +13,7 @@ const historyList = [
     <div class="prof-basic-contents-wrap">
       <img src="../assets/profile/myAvatar.jpg" class="prof-basic-my-avatar" />
       <h1>Yuma Mishima</h1>
-      <p>システム開発で笑顔を増やせたらと考えています。</p>
+      <p>システム開発で笑顔を<br />増やせたらと考えています。</p>
     </div>
     <div class="prof-basic-histories-wrap">
       <h1>取得した資格</h1>
@@ -22,6 +22,21 @@ const historyList = [
           <img :src="list.img" alt="list-icon" class="history-img" />{{ list.title }}
         </li>
       </ul>
+    </div>
+    <div class="prof-basic-histories-wrap gradient">
+      <h1>技術者を目指した切っ掛け</h1>
+      <img src="../assets/profile/undraw_back_in_the_day_knsh.svg" class="img-class" />
+      <p>"ゲーム"が切っ掛けでプログラミングを知りました。</p>
+      <p>そこから"疑問の答えを求めて"<br />気が付くとSEになっていました。</p>
+    </div>
+    <div class="prof-basic-histories-wrap gradient-reverse">
+      <img src="../assets/profile/undraw_programmer_re_owql.svg" class="img-class" />
+    </div>
+    <hr />
+    <div class="prof-basic-histories-wrap">
+      <h1>趣味</h1>
+      <img src="../assets/profile/tp-icon.jpg" class="tp-icon-img" />
+      <h2>トランペット🎺</h2>
     </div>
     <hr />
     <nav class="prof-basic-nav-wrap">
@@ -46,6 +61,7 @@ article {
 .prof-basic-contents-wrap p {
   font-size: large;
   font-weight: bolder;
+  padding: 0 0.5rem;
 }
 
 .prof-basic-my-avatar {
@@ -55,6 +71,7 @@ article {
 }
 
 .prof-basic-histories-wrap {
+  margin-top: 2rem;
   padding: 0 1rem;
 }
 
@@ -62,7 +79,7 @@ ul {
   display: inline-table;
   text-align: left;
   list-style-type: none;
-  font-size: x-large;
+  font-size: large;
   font-weight: bold;
 }
 
@@ -82,5 +99,25 @@ hr {
 
 .prof-basic-nav-wrap {
   padding: 0;
+}
+
+.img-class {
+  width: 20rem;
+}
+
+.gradient {
+  background: linear-gradient(white, #ffe227);
+  padding-bottom: 1rem;
+}
+
+.gradient-reverse {
+  margin: 0;
+  padding: 0;
+  background: linear-gradient(#ffe227, white);
+}
+
+.tp-icon-img {
+  width: 7rem;
+  border-radius: 50%;
 }
 </style>
