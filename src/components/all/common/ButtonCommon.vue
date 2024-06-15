23d3ea50da4598ab3cd06dfa89279eb8c055549e
bignrn <raiyo2580@gmail.com>
2024-06-15 13:56:50 +0900

    カラーコードを変数に変更

--- src/components/all/common/ButtonCommon.vue
+++ src/components/all/common/ButtonCommon.vue
@@ -24,10 +24,10 @@ defineProps({
   border: $button-bg solid 0.1rem;
   font-size: 1.6rem;
   font-weight: bold;
-  color: black;
+  color: $black;
   padding: 0.3rem 1rem;
   border-radius: 1rem;
-  box-shadow: 0rem 0.1rem 0.1em black;
+  box-shadow: 0rem 0.1rem 0.1em $black;
   box-sizing: content-box;
   cursor: pointer;
 
@@ -41,9 +41,9 @@ defineProps({
 
   &:active {
     cursor: pointer;
-    background-color: rgb(169, 135, 73);
-    border-color: rgb(169, 135, 73);
-    box-shadow: 0rem 0rem 0.1em black;
+    background-color: $button-active-color;
+    border-color: $button-active-color;
+    box-shadow: 0rem 0rem 0.1em $black;
   }
 }
 </style>
