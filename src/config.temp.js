6f894dd09953411e7ea75822a00e3ebc70d68c34
bignrn <raiyo2580@gmail.com>
2025-05-22 09:51:40 +0900

    firebaseのfirestoreの設定

---
+++ src/config.temp.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "YOUR_TOKEN",
+  authDomain: "YOUR_DOMAIN",
+  databaseURL: "YOUR_DB",
+  projectId: "YOUR_PJ",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_ID"
+};
+
+export { firebaseConfig };
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "@/config";
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
 
 const app = createApp(App)
 app.use(createPinia())
